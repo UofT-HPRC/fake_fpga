5a687e1fdc7dd0aa6395870e3b155785477ca8a0
unknown <mahkoe@gmail.com>
2020-09-05 15:27:55 -0400

    Oops forgot to commit that I moved these files

--- u_of_t_scripts/run_sim_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-vlib work
-vlog tb.v
-vlog main.vo
-vsim -pli fakefpga.vpi -novopt -c -t 1ps -L cyclonev_ver -L altera_ver -L altera_mf_ver -L 220model_ver -L sgate_ver -L altera_lnsim_ver tb -voptargs="+acc" -do "run -all"
